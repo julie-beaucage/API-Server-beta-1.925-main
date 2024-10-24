ddc79e11d64bb5abdf327db411a38652d83de7fc
julie-beaucage <201386196@edu.clg.qc.ca>
2024-10-24 10:19:23 -0400

    ok

--- cachedRequestsManager.js
+++ cachedRequestsManager.js
@@ -1,50 +1,57 @@
 import * as utilities from "./utilities.js";
 import * as serverVariables from "./serverVariables.js";
 
-let cacheRequestsExpirationTime = serverVariables.get("main.cacheRequestsExpirationTime");
+let cacheRequestsExpirationTime = serverVariables.get(
+  "main.cacheRequestsExpirationTime"
+);
 
 // Repository file data urls cache
 global.requestsCache = [];
 global.cachedRequestsCleaner = false;
 
 export default class CachedRequestsManager {
-  static startCachedRequestsCleaner() {
-    /* démarre le processus de nettoyage des caches périmées */
-    setInterval(CachedRequestsManager.flushExpired, cacheRequestsExpirationTime * 1000);
-    console.log(BgWhite + FgOrange, "[Periodic repositories data caches cleaning process started...]");
-  }
   static add(url, content, ETag = "") {
+    /* mise en cache */
     if (!cachedRequestsCleaner) {
       cachedRequestsCleaner = true;
       CachedRequestsManager.startCachedRequestsCleaner();
-  }
-  if (url != "") {
+    }
+    if (url != "") {
       CachedRequestsManager.clear(url);
       requestsCache.push({
-          url,
-          content,
-          ETag,
-          Expire_Time: utilities.nowInSeconds() + cacheRequestsExpirationTime
+        url,
+        content,
+        ETag,
+        Expire_Time: utilities.nowInSeconds() + cacheRequestsExpirationTime,
       });
-      console.log(BgWhite + FgOrange, `[Data of url ${url} repository has been cached]`);
+      console.log(BgWhite + FgOrange,  `[Data of url:  ${url} repository has been cached]` );
+    }
   }
+  static startCachedRequestsCleaner() {
+    /* démarre le processus de nettoyage des caches périmées */
+    setInterval(
+      CachedRequestsManager.flushExpired,
+      cacheRequestsExpirationTime * 1000
+    );
+    console.log(BgWhite + FgOrange,"[Periodic repositories data caches url cleaning process started...]");
   }
   static find(url) {
-    /* si existe, si existe pas return null, retourne la cache associée à l'url */
+    /* retourne la cache associée à l'url */
+   console.log(requestsCache);
     try {
       if (url != "") {
-          for (let cache of requestsCache) {
-              if (cache.url == url) {
-                  cache.Expire_Time = utilities.nowInSeconds() + cacheRequestsExpirationTime;
-                  console.log(BgWhite + FgOrange, `[${cache.url} data url retrieved from cache]`);
-                  return cache.data;
-              }
+        for (let cache of requestsCache) {
+          if (cache.url == url) {
+            cache.Expire_Time = utilities.nowInSeconds() + cacheRequestsExpirationTime;
+            console.log(BgWhite + FgOrange, `[${cache.url} data url retrieved from cache]`);
+            return cache;
           }
+        }
       }
-  } catch (error) {
-      console.log(BgWhite + FgRed, "[repository cache error!]", error);
-  }
-  return null;
+    } catch (error) {
+      console.log(BgWhite + FgRed, "[repository cache url error!]", error);
+    }
+    return null;
   }
   static clear(url) {
     /* efface la cache associée à l’url */
@@ -52,35 +59,34 @@ export default class CachedRequestsManager {
       let indexToDelete = [];
       let index = 0;
       for (let cache of requestsCache) {
-          if (cache.url== url) indexToDelete.push(index);
-          index++;
+        if (cache.url == url) indexToDelete.push(index);
+        index++;
       }
       utilities.deleteByIndex(requestsCache, indexToDelete);
-  }
+    }
   }
   static flushExpired() {
     /* efface les caches expirées */
     let now = utilities.nowInSeconds();
     for (let cache of requestsCache) {
-        if (cache.Expire_Time <= now) {
-            console.log(BgWhite + FgOrange, "Cached file data of url: " + cache.url + ".json expired");
-        }
+      if (cache.Expire_Time <= now) {
+        console.log(BgWhite + FgOrange,"Cached file data of url: " + cache.url + ".json expired" );
+      }
     }
-    requestsCache = requestsCache.filter( cache => cache.Expire_Time > now);
+    requestsCache = requestsCache.filter((cache) => cache.Expire_Time > now);
   }
-static get(HttpContext) {
+  static get(HttpContext) {
     /* 
     Chercher la cache correspondant à l'url de la requête. Si trouvée,
     envoyer la réponse avec HttpContext.response.JSON(content, ETag, true /* from cache).
     Donc, si les données proviennent du cache, on ne les met pas dans une autre cache. 
     Si elles ne proviennent pas du cache, il faut les ajouter.
     */
-        const cache = CachedRequestsManager.find(HttpContext.req.url);
-        if (cache && HttpContext.isCacheable) {
-            cache.Expire_Time = utilities.nowInSeconds() + cacheRequestsExpirationTime;
-            HttpContext.response.JSON(cache.content, cache.ETag, true);
-    };
-   }
-
+    let cache = CachedRequestsManager.find(HttpContext.req.url);
+    if (cache && HttpContext.isCacheable) {
+      HttpContext.response.JSON(cache.content, cache.ETag, true);
+      return true;
+    }
+    return false;
+  }
 }
-
